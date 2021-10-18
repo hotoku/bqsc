2d77d3c38c8ec0ee8cacdfb897f0aa1a96832c68
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2021-10-18 11:17:37 +0900

    Bump version: 1.2.1 → 1.2.2

--- bqsc/__init__.py
+++ bqsc/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "1.2.1"
+__version__ = "1.2.2"
 
 from .load import load as load, loads as loads, load_dir as load_dir
 from .table import TypeMismatch as TypeMismatch, NotDefinedColumn as NotDefinedColumn
