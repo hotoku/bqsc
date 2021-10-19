954f8204dbe8df8e4915bee3376e15c9372fc47d
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2021-10-19 10:18:51 +0900

    readme

--- sample/main.py
+++ sample/main.py
@@ -1,4 +1,10 @@
 import table_schema as ts
 
 pred = ts.Prediction()
-pred.exec_id = [1, 2, 3]  # => TypeMismatch exception
+pred.exec_id = "a"
+
+list_str = ["a", "b", "c"]
+list_int = [1, 2, 3]
+pred.forward_week_num = list_int
+
+pred.product_code = list_int
