5b69b3345b99e940034f148acc86409859f53943
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2021-10-19 10:19:44 +0900

    export dataframe

--- bqsc/__init__.py
+++ bqsc/__init__.py
@@ -1,4 +1,9 @@
 __version__ = "2.1.1"
 
 from .load import load as load, loads as loads, load_dir as load_dir
-from .table import Table as Table, TypeMismatch as TypeMismatch, NotDefinedColumn as NotDefinedColumn
+from .table import (
+    Table as Table,
+    TypeMismatch as TypeMismatch,
+    NotDefinedColumn as NotDefinedColumn,
+    dataframe as dataframe
+)
