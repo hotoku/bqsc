ca2860af0c559ecc2f1106154407ae635f3dadd7
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2021-10-21 17:41:01 +0900

    fix

--- .github_scripts/pyright.sh
+++ .github_scripts/pyright.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -l
 
 
-poetry run node node_modules/pyright/index.js di_predict tests
+poetry run node node_modules/pyright/index.js ${GITHUB_REPOSITORY_NAME_SLUG} tests
 echo ::set-output name=exit-code::$?
