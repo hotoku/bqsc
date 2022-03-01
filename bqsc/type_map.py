adc50ba4a304b329593acf32a85133f626591f7e
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2022-03-01 09:01:54 +0900

    add int64, float64

--- bqsc/type_map.py
+++ bqsc/type_map.py
@@ -5,7 +5,9 @@ from datetime import date, datetime, time, timedelta
 TYPE_MAP: Dict[str, type] = {
     "string": str,
     "integer": int,
+    "int64": int,
     "float": float,
+    "float64": float,
     "boolean": bool,
     "date": date,
     "datetime": datetime,
