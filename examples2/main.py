8671a168755b1754d056c2e96e60be6405bf82a5
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2022-03-15 19:59:08 +0900

    ex2

--- examples2/main.py
+++ examples2/main.py
@@ -1,4 +1,10 @@
-import schemas
+import bqsc
+
+from . import schemas
 
 my_table = schemas.MyTable()
-my_table.col_int = "abracadabra"
+my_table.col_int = [1, 2, 3]
+my_table.col_float = [4.0, 5.0, 6.0]
+my_table.col_str = ["a", "b", "c"]
+
+df = bqsc.dataframe(my_table)
